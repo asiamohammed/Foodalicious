65a0f9bd1a5f635ccc982dac3a4e536812e84f3f
asiamohammed <mohamedasia021@gmail.com>
2022-10-09 22:57:21 +0300

    Final commit

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,6 @@
 import Home from "./Home";
 
 import Muffins from "../components/Muffins";
-import Noodels from "../components/Muffins";
 import Curry from "../components/Curry";
 import { Route, Routes} from "react-router-dom";
 import Searched from "./Searched";
@@ -13,7 +12,6 @@ function Pages() {
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/Muffins" element= {<Muffins/>}/>
-    <Route path="/Noodels" element= {<Noodels/>}/>
     <Route path="/Searched/:Search" element= {<Searched/>}/>
     <Route path="/Recipe/" element= {<Recipe/>}/>
     <Route path="/Curry" element= {<Curry/>}/>
